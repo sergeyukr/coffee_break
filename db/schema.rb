0ac4a71c3f1d1bd567d5428dc42a59df9b8777a3
andrewjek <andrewjek@yandex.ua>
2013-01-02 23:17:46 +0200

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102162439) do
+ActiveRecord::Schema.define(:version => 20130102210836) do
 
   create_table "bakings", :force => true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130102162439) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "order_id"
   end
 
   create_table "items", :force => true do |t|
